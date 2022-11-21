9fc53d4d7d33494f3cb9a65c9b7823820a84bf81
BradChao <brad@brad.tw>
2022-11-21 14:06:30 +0800

    11211406

---
+++ brad04.php
@@ -0,0 +1,12 @@
+<?php
+    $var1 = 123;    // integer
+    $var2 = 12.3;   // double
+    $var3 = true;   // boolean
+    $var4 = 'Brad'; // string
+    echo gettype($var1);    // => type string
+?>
+<hr />
+<?php
+    $var1 = 'ispan';
+    echo gettype($var1);
+?>
---
+++ brad05.php
@@ -0,0 +1,12 @@
+<?php
+    $var1 = 10; $var2 = 3;
+    $var3 = $var1 + $var2;
+    echo $var1 . ' + ' . $var2 . ' = ' . $var3 . '<br />';
+    echo "$var1 + $var2 = $var3<br />";
+    echo '$var1 + $var2 = $var3<br />';
+    
+    $weight = 80;
+    echo "Brad Weight = {$weight}kg";
+?>
+<hr />
+Hello, World
