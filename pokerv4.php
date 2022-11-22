08e08c3973248f320c3fdfa85dabe40be0b56daa
BradChao <brad@brad.tw>
2022-11-22 15:55:28 +0800

    11221555

---
+++ pokerv4.php
@@ -0,0 +1,21 @@
+<?php
+    $poker = range(0,51);
+    shuffle($poker);
+
+    foreach($poker as $card){
+        echo "{$card}<br />";
+    }    
+?>
+<hr />
+<?php
+    $players = [[],[],[],[]];
+    foreach($poker as $i => $card){
+        $players[$i%4][(int)($i/4)] = $card;
+    }
+
+    foreach($players[1] as $card){
+        echo "{$card}<br />";
+    }
+
+
+?>
