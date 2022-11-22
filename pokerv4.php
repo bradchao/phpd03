6a90359124f3433053f8cb1740b5dd5239648b49
BradChao <brad@brad.tw>
2022-11-22 16:31:45 +0800

    11221631

--- pokerv4.php
+++ pokerv4.php
@@ -16,15 +16,28 @@
     // foreach($players[1] as $card){
     //     echo "{$card}<br />";
     // }
+
 ?>
+<!-- &spades; &hearts;  &diams; &clubs;  -->
 <table border="1" width="100%">
     <?php
+        $pattern = ['&spades;','<font color=red>&hearts;</font>',
+        '<font color=red>&diams;</font>','&clubs;'];
+        $value = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
         foreach($players as $player){
+            //sort($player);
             echo '<tr>';
             foreach($player as $card){
-                echo "<td>{$card}</td>";
+                $p = $pattern[(int)$card/13];
+                $v = $value[$card%13];
+                echo "<td>{$p}{$v}</td>";
             }
             echo '</tr>';    
         }
+
+        function mysort(&$ary){
+            
+        }
+
     ?>
 </table>
