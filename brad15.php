7b33ab278ecdb43ac88d0bda6eb1396571ff3bcb
BradChao <brad@brad.tw>
2022-11-22 11:14:10 +0800

    11221114

--- brad15.php
+++ brad15.php
@@ -1,40 +1,17 @@
 <table border='1' width='100%'>
-    <tr>
-        <?php
-            for ($j = 2; $j <= 5; $j++){
-                echo '<td>';
-                for($i = 1; $i <= 9; $i++){
-                    $r = $j * $i;
-                    echo "{$j} x {$i} = {$r}<br />";
-                }
-                echo '</td>';    
+<?php
+    for($k = 0; $k < 2; $k++){
+        echo '<tr>';
+        for ($j = 2; $j <= 5; $j++){
+            $newj = $j + $k * 4;
+            echo '<td>';
+            for($i = 1; $i <= 9; $i++){
+                $r = $newj * $i;
+                echo "{$newj} x {$i} = {$r}<br />";
             }
-        ?>
-    </tr>
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
+            echo '</td>';    
+        }
+        echo '</tr>';
+    }
+?>
 </table>
