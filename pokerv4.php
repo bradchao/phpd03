69405baacec5e7470f9018a337fed5141f1882ab
BradChao <brad@brad.tw>
2022-11-23 09:21:33 +0800

    11230921

--- pokerv4.php
+++ pokerv4.php
@@ -26,6 +26,8 @@
         $value = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
         foreach($players as $player){
             //sort($player);
+            $player = mysort($player);
+            
             echo '<tr>';
             foreach($player as $card){
                 $p = $pattern[(int)$card/13];
@@ -35,8 +37,18 @@
             echo '</tr>';    
         }
 
-        function mysort(&$ary){
-            
+        function mysort($ary){
+            $temp = array();
+
+            for ($j = 0; $j<13; $j++){
+                for ($i=0; $i<count($ary);$i++){
+                    if ($ary[$i] % 13 == $j){
+                        $temp[] = $ary[$i];
+                    }
+                }
+            }
+
+            return $temp;
         }
 
     ?>
