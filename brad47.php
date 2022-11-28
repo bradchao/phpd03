dfdbe870236d490b981da48ec8a72f7d28fe908f
BradChao <brad@brad.tw>
2022-11-28 12:04:08 +0800

    11281204

--- brad47.php
+++ brad47.php
@@ -2,14 +2,28 @@
 <?php
     $mysqli = new mysqli(
         'localhost', 'root', 'root', 'phpd03', 3306) ;
-    $sql = 
-        'INSERT INTO cust (cname,tel,birthday) ' . 
-            'VALUES ("brad","123","1999-01-02")';
-    if ($mysqli->query($sql)){
-        // success
+
+    // $sql = 
+    //     'INSERT INTO cust (cname,tel,birthday) ' . 
+    //         'VALUES ("brad","123","1999-01-02")';
+    // if ($mysqli->query($sql)){
+    //     // success
+    // }else{
+    //     // failure
+    // }
+
+    $name = 'Brad'; $tel = '123456'; $birthday = '1999-03-04';
+    $sql = 'INSERT INTO cust (cname,tel,birthday) VALUES (?,?,?)';
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('sss', $name, $tel, $birthday);
+    if ($stmt->execute()){
+        echo 'success';
     }else{
-        // failure
+        echo 'failure';
     }
+
+
+
 
     
 
