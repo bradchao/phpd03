6fc9966499e7c87e090b6c318338e5cae468016e
BradChao <brad@brad.tw>
2022-11-29 14:42:35 +0800

    11291442

--- register.php
+++ register.php
@@ -1,9 +1,24 @@
-<?php
+Register Page
+<hr />
+<script>
+    const xhttp = new XMLHttpRequest();
+    function checkNewAccount(){
+        xhttp.onreadystatechange = afterCheck;
+        xhttp.open("GET", "isNewAccount.php", true);
+        xhttp.send();
+    }
+    function afterCheck(){
+        if (xhttp.readyState == 4 && xhttp.status == 200){
 
-?>
+        }
+    }
+
+</script>
 <form method="post" enctype="multipart/form-data">
-    Account:<input type="text" name="account" /><br />
-    Password:<input type="text" name="passwd" /><br />
+    Account:<input type="text" name="account" 
+        onchange="checkNewAccount()"/><br />
+    Password:<input type="text" name="passwd" />
+    <span id='mesg'></span><br />
     Realname:<input type="text" name="realname" /><br />
     Icon:<input type="file" name="icon" /><br />
     <input type="submit" value="Register" />
