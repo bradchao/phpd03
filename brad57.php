d89fbb70b374379a01c9aca0a023105ddc383346
BradChao <brad@brad.tw>
2022-11-29 09:20:25 +0800

    11290920

--- brad57.php
+++ brad57.php
@@ -9,19 +9,19 @@
         $stmt = $mysqli->prepare($sql);
         $stmt->bind_param('s', $account);
         $stmt->execute();
+        $stmt->store_result();
         if ($stmt->num_rows() != 0){
-            $stmt->store_result();
-            $stmt->bind_result($account,$passwdHash,$realname);
+            $stmt->bind_result($acc,$passwdHash,$realname);
+            $stmt->fetch();
             if (password_verify($passwd, $passwdHash)){
-
+                header("Location: brad58.php");
             }else{
-
+                header("Location: brad56.php");
             }
 
         } else{
             header('Location: brad56.php');
         }
-
     }else{
         header('Location: brad56.php');
     }
