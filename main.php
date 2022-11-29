4dfa733fbdebf89e0874f147f1c641b3219fc12f
BradChao <brad@brad.tw>
2022-11-29 16:10:25 +0800

    11291610

--- main.php
+++ main.php
@@ -2,7 +2,16 @@
     session_start();
     if (!isset($_SESSION['member'])) header('Location: login.php');
     $member = $_SESSION['member'];
+
+    $icon = base64_encode($member->icon); 
+
 ?>
 <h1>Brad Big Company</h1>
 <hr />
-Welcome, <?php echo $member->realname; ?>
+Welcome, <?php echo $member->realname; ?><br />
+<!-- data:type;base64, xxx -->
+<img width='32px' height='32px' 
+    src="data:<?php echo $member->icontype; ?>;base64, 
+    <?php echo $icon; ?>" />
+
+
